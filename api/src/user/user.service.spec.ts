decbeaae72fbe8350dc122711e9b890c6266fa99
DonatasD <donatasdaubar@gmail.com>
2021-01-10 12:16:26 +0200

    Add user.service.spec tests

--- api/src/user/user.service.spec.ts
+++ api/src/user/user.service.spec.ts
@@ -1,10 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
-import { mockInvalidUserId, mockUser, mockUsers, userRepositoryMock } from './user.mock';
+import {
+  mockInvalidUserId,
+  mockUser,
+  mockUserCreateDto,
+  mockUsers,
+  mockUserUpdateDto,
+  userRepositoryMock,
+} from './user.mock';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { MockType } from '../utils/mock/mock.types';
+import faker from 'faker';
 
 describe('UserService', () => {
   let service: UserService;
@@ -30,36 +38,63 @@ describe('UserService', () => {
   });
 
   describe('findAll()', () => {
-    it('should successfully return users', () => {
+    it('should successfully return users', async () => {
       const expected = mockUsers();
       repository.find.mockReturnValue(expected);
-      const result = service.findAll();
+      const result = await service.findAll();
       expect(repository.find).toBeCalled();
       expect(result).toEqual(expected);
     });
 
-    it('should successfully return no users', () => {
+    it('should successfully return no users', async () => {
       const expected = [];
       repository.find.mockReturnValue(expected);
-      const result = service.findAll();
+      const result = await service.findAll();
       expect(repository.find).toBeCalled();
       expect(result).toEqual(expected);
     });
   });
 
-  describe('findOne()', () => {
-    it('should successfully return user', () => {
+  describe('findOne()',  () => {
+    it('should successfully return user', async () => {
       const expected = mockUser();
       repository.findOne.mockReturnValue(expected);
-      const result = service.findOne(expected.id);
+      const result = await service.findOne(expected.id);
       expect(repository.findOne).toBeCalled();
       expect(result).toEqual(expected);
     });
 
-    it('should find no user', () => {
-      const result = service.findOne(mockInvalidUserId());
+    it('should find no user', async () => {
+      const result = await service.findOne(mockInvalidUserId());
       expect(repository.findOne).toBeCalled();
       expect(result).toEqual(null);
+    });
+  });
+
+  describe('create()', () => {
+    it('should successfully create a user', async () => {
+      const expected = mockUserCreateDto();
+      const result = await repository.create(expected);
+      expect(repository.create).toBeCalled();
+      expect(result).toEqual(expect.objectContaining(expected));
+    });
+  });
+
+  describe('update()', () => {
+    it('should successfully update a user', async () => {
+      const expected = mockUserUpdateDto();
+      const id = faker.random.uuid();
+      const result = await repository.update(id, expected);
+      expect(repository.update).toBeCalled();
+      expect(result).toEqual(expect.objectContaining(expected));
+    });
+  });
+
+  describe('remove()', () => {
+    it('should successfully delete user', async () => {
+      const id = faker.random.uuid();
+      await repository.remove(id);
+      expect(repository.remove).toBeCalled();
     });
   });
 });
